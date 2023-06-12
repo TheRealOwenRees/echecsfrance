ec60b6be91100e1592cdc41ecdc25e40ad36bcd0
Owen Rees <owenrees82@gmail.com>
2023-06-12 16:56:51 +0200

    conditional base url for prod and dev

--- utils/getTournamentData.tsx
+++ utils/getTournamentData.tsx
@@ -3,7 +3,10 @@
  * @remarks The result is cached for the revalidation period in seconds
  */
 export default async function getTournaments(country: string) {
-  const res = await fetch(`http://localhost:3000/api/tournaments/${country}`, {
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
+  const res = await fetch(`${baseUrl}/api/tournaments/${country}`, {
     next: { revalidate: 300 },
   });
   return await res.json();
