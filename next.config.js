db266e6ef339f5c08779879eff6d5ba33ad5120f
Owen Rees <owenrees82@gmail.com>
2023-06-13 10:47:55 +0200

    env update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const dotenvExpand = require("dotenv-expand");
+
+dotenvExpand.expand({ parsed: { ...process.env } });
 const nextConfig = {};
 
 module.exports = nextConfig;
