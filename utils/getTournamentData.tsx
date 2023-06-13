678fef42c0b429bff7baf565c463a6ebf1a4ed3a
Owen Rees <owenrees82@gmail.com>
2023-06-13 09:28:06 +0200

    env revert

--- utils/getTournamentData.tsx
+++ utils/getTournamentData.tsx
@@ -3,9 +3,7 @@
  * @remarks The result is cached for the revalidation period in seconds
  */
 export default async function getTournaments(country: string) {
-  const server = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? process.env.NEXT_PUBLIC_VERCEL_URL
-    : process.env.SERVER;
+  const server = process.env.SERVER;
 
   const res = await fetch(`${server}/api/tournaments/${country}`, {
     next: { revalidate: 300 },
