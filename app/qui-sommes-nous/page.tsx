daa38f16f99edebb59da025a7f6ff7c83f1309b9
Owen Rees <owenrees82@gmail.com>
2023-06-28 20:53:50 +0200

    font size and max width changes

--- app/qui-sommes-nous/page.tsx
+++ app/qui-sommes-nous/page.tsx
@@ -4,21 +4,21 @@ import Layout from "@/components/Layout";
 export default function About() {
   return (
     <Layout>
-      <header className="grid h-[calc(100%-216px)] md:h-[calc(100%-174px)] place-items-center">
-        <div className="h-full px-4 pt-8 pb-12 bg-white dark:bg-gray-800">
+      <header className="grid place-items-center bg-white dark:bg-gray-800">
+        <div className="h-full max-w-5xl px-4 pt-8 pb-12 bg-white dark:bg-gray-800">
           <h2
-            className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+            className="mb-8 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
             data-test="header2"
           >
             Qui Sommes-Nous?
           </h2>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          <p className="mb-4 font-light text-center text-gray-500 dark:text-gray-400 md:text-xl">
             This project was first started in early 2022 as a means to visualise
             where chess tournaments were taking place in France. Having moved to
             France in 2019, I did not know my way around the country and wanted
             to know which tournaments were happening near me.
           </p>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          <p className="mb-4 font-light text-center text-gray-500 dark:text-gray-400 md:text-xl">
             The project was shelved until Spring 2023 when it was given a new
             lease of life. Rebuilt from the ground up,{" "}
             <Link href="/" className="text-teal-600">
@@ -26,85 +26,85 @@ export default function About() {
             </Link>{" "}
             is now open source and open to contributions.
           </p>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          <p className="mb-4 font-light text-center text-gray-500 dark:text-gray-400 md:text-xl">
             I will be setting up a donation link if you wish to help cover some
             of the costs of running this website. After costs, and remaining
             funds will put back into the chess community by means of sponsoring
             events or by donations.
           </p>
           <h3
-            className="mb-4 text-2xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+            className="mt-8 mb-4 text-2xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
             data-test="header2"
           >
             Credits
           </h3>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          <p className="mb-4 font-light text-center text-gray-500 dark:text-gray-400 md:text-xl">
             This project is what it is thanks to the contributions of the wider
             community. In particular I would like to thank the following for
             their contributions so far:
-            <ul className="text-teal-600">
-              <li>
-                <Link href="#" target="_blank">
-                  Alvaro
-                </Link>
-              </li>
-              <li>
-                <Link href="#" target="_blank">
-                  Flo
-                </Link>
-              </li>
-            </ul>
           </p>
+          <ul className="mb-4 text-center text-teal-600">
+            <li>
+              <Link href="#" target="_blank">
+                Alvaro
+              </Link>
+            </li>
+            <li>
+              <Link href="#" target="_blank">
+                Flo
+              </Link>
+            </li>
+          </ul>
           <h3
-            className="mb-4 text-2xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+            className="mt-8 mb-4 text-2xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
             data-test="header2"
           >
             Tech Blurb
           </h3>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          <p className="mb-4 font-light text-center text-gray-500 dark:text-gray-400 md:text-xl">
             <Link href="/" className="text-teal-600">
               Echecs France
             </Link>{" "}
             is built on the following tech stack:
-            <ul className="flex justify-around text-teal-600">
-              <li>
-                <Link href="https://nextjs.org/" target="_blank">
-                  NextJS
-                </Link>
-              </li>
-              <li>
-                <Link href="#" target="_blank">
-                  Typescript
-                </Link>
-              </li>
-              <li>
-                <Link href="https://tailwindcss.com/" target="_blank">
-                  Tailwind
-                </Link>
-              </li>
-              <li>
-                <Link href="https://mongodb.com/" target="_blank">
-                  MongoDB
-                </Link>
-              </li>
-            </ul>
           </p>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          <ul className="mb-4 flex justify-around text-teal-600">
+            <li>
+              <Link href="https://nextjs.org/" target="_blank">
+                NextJS
+              </Link>
+            </li>
+            <li>
+              <Link href="#" target="_blank">
+                Typescript
+              </Link>
+            </li>
+            <li>
+              <Link href="https://tailwindcss.com/" target="_blank">
+                Tailwind
+              </Link>
+            </li>
+            <li>
+              <Link href="https://mongodb.com/" target="_blank">
+                MongoDB
+              </Link>
+            </li>
+          </ul>
+          <p className="mb-4 font-light text-center text-gray-500 dark:text-gray-400 md:text-xl">
             with the following core dependencies:
-            <ul className="flex justify-around text-teal-600">
-              <li>
-                <Link href="https://leafletjs.com/" target="_blank">
-                  Leaflet
-                </Link>
-              </li>
-              <li>
-                <Link href="https://nodemailer.com/" target="_blank">
-                  Nodemailer
-                </Link>
-              </li>
-            </ul>
           </p>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          <ul className="mb-4 flex justify-around text-teal-600">
+            <li>
+              <Link href="https://leafletjs.com/" target="_blank">
+                Leaflet
+              </Link>
+            </li>
+            <li>
+              <Link href="https://nodemailer.com/" target="_blank">
+                Nodemailer
+              </Link>
+            </li>
+          </ul>
+          <p className="mb-16 font-light text-center text-gray-500 dark:text-gray-400 md:text-xl">
             For more information and details on how to contribute, please visit
             our{" "}
             <Link
@@ -112,10 +112,9 @@ export default function About() {
               target="_blank"
               className="text-teal-600"
             >
-              GitHub repository
+              GitHub repository.
             </Link>
           </p>
-          .
         </div>
       </header>
     </Layout>
