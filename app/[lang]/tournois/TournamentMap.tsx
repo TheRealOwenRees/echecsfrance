fc0fa4d0c43f305f1df453aaf6953acd46b55d6c
Owen Rees <owenrees82@gmail.com>
2023-07-18 21:25:29 +0200

    center on France and stay within bounds on initial load

--- app/[lang]/tournois/TournamentMap.tsx
+++ app/[lang]/tournois/TournamentMap.tsx
@@ -51,14 +51,21 @@ declare class BouncingMarker extends Marker {
 
 const MapEvents = () => {
   const setMapBounds = useSetAtom(mapBoundsAtom);
+
+  const worldBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
+  const franceBounds = L.latLngBounds(
+    L.latLng(42.08, -5.12),
+    L.latLng(51.17, 9.53),
+  );
+
   const map = useMapEvent("moveend", () => {
     // Set the map bounds atoms when the user pans/zooms
     setMapBounds(map.getBounds());
   });
 
-  // Set the maximum bounds of the map to prevent moving past the world
+  // viewport agnostic centering of France & max world bounds
   useEffect(() => {
-    const worldBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
+    map.setView(franceBounds.getCenter(), map.getBoundsZoom(franceBounds));
     map.setMaxBounds(worldBounds);
     map.options.maxBoundsViscosity = 1.0; // Prevents going past bounds while dragging
   }, [map]);
