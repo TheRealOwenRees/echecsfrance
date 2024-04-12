18a80a942e39152849cf5d75b7b6773f7abed54e
Timothy Armes <tim@timothyarmes.com>
2024-04-12 14:54:44 +0200

    Avatar with drop down menu for accessing zones and signing out

--- src/components/LoadingMap.tsx
+++ src/components/LoadingMap.tsx
@@ -1,10 +1,12 @@
 import { useTranslations } from "next-intl";
 
+import { Spinner } from "./Spinner";
+
 const LoadingMap = () => {
   const t = useTranslations("Tournaments");
   return (
-    <div className="grid h-content place-self-center bg-white text-center text-gray-900 dark:bg-gray-800 dark:text-white">
-      <p>{t("loading")}</p>
+    <div className="mt-20 flex h-content justify-center bg-white pt-20 text-center text-gray-900 dark:bg-gray-800 dark:text-white">
+      <Spinner />
     </div>
   );
 };
